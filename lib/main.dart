24e2bfa8b4c22c108bfa6940c22d18e3be0147b7
Thinh Le <letienthinh0109@gmail.com>
2023-05-23 20:09:35 +0700

    done theme & splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:permix/screen/login-screen.dart';
+import 'package:permix/screen/splash-screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -6,15 +9,62 @@ void main() {
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
+  static const BUTTON_WIDTH_RATIO = 0.38;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    var size = MediaQuery.of(context).size;
+    return MaterialApp(
+      title: "Permix",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        actionIconTheme: const ActionIconThemeData(),
+        badgeTheme: BadgeThemeData(),
+        fontFamily: "Lato",
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            fixedSize: MaterialStateProperty.all(
+              Size.fromWidth(BUTTON_WIDTH_RATIO * size.width),
+            ),
+            shape: MaterialStateProperty.all(
+              LinearBorder(side: BorderSide.none),
+            ),
+            textStyle: MaterialStateProperty.all(
+              TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+            ),
+          ),
         ),
+        textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+            fixedSize: MaterialStateProperty.all(
+              Size.fromWidth(BUTTON_WIDTH_RATIO * size.width),
+            ),
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                side: BorderSide(
+                  color: Color.fromRGBO(220, 207, 102, 1),
+                  // Theme.of(context).colorScheme.primary,
+                  //this cause error
+                  width: 1,
+                ),
+              ),
+            ),
+          ),
+        ),
+        colorScheme: const ColorScheme(
+            primary: Color.fromRGBO(220, 207, 102, 1),
+            onPrimary: Colors.black,
+            secondary: Color.fromRGBO(127, 105, 81, 1),
+            onSecondary: Colors.black,
+            background: Color.fromRGBO(50, 50, 50, 1), //#323232
+            brightness: Brightness.light,
+            error: Colors.red,
+            onError: Colors.red,
+            onBackground: Colors.green,
+            surface: Colors.orange,
+            onSurface: Colors.black12),
       ),
+      home: Scaffold(body: LoginScreen()),
     );
   }
 }
---
+++ lib/screen/splash-screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
+    return Container(
+      alignment: Alignment.center,
+      color: Theme.of(context).colorScheme.background,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset("assets/images/logo-main.png"),
+          SizedBox(
+            height: 1 / 6 * size.height,
+          ),
+          ElevatedButton(onPressed: () {}, child: Text('Login')),
+          TextButton(onPressed: () {}, child: Text('Create Account'))
+        ],
+      ),
+    );
+  }
+}
